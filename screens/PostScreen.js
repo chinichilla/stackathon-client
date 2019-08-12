19e648d305c4bb084e5017bc68f501bc051c433b
djchinia <djchinia@gmail.com>
2019-08-12 09:09:17 -0400

    functional cod

---
+++ screens/PostScreen.js
@@ -0,0 +1,68 @@
+import React, { Fragment, Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Geolocation from '@react-native-community/geolocation';
+
+export default class PostScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      latitude: 0,
+      longitude: 0,
+    };
+  }
+
+  componentDidMount() {
+    Geolocation.getCurrentPosition(position => {
+      this.setState({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView
+          // maybe switch to google maps here using provider
+          style={styles.map}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          showsUserLocation={true}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    // height: 300,
+    // width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
